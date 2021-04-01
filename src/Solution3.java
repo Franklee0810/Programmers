91cd2b29eb3a31f99be4221b86872a1aff6d5c10
Frank_lee <73864337+Franklee0810@users.noreply.github.com>
2021-04-01 17:47:11 +0900

    추가솔루션3

--- src/Solution3.java
+++ src/Solution3.java
@@ -1,11 +1,14 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Solution3 {
 	
 	public String solution(String[] participant, String[] completion) {
-		public String solution(String[] participant, String[] completion) {
+		
 
 	        Map<String, Long> participantMap = Arrays.asList(participant).stream()
 	                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
