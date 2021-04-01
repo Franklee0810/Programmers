31e98e8e28697879462ef5398e1e952db8384b09
Frank_lee <73864337+Franklee0810@users.noreply.github.com>
2021-04-01 12:04:06 +0900

    결국 못풀었음 30분동안..

---
+++ src/Exam1.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class Exam1 {
+ 
+	public static void main(String[] args) {
+//		수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+//
+//		마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+//
+//		제한사항
+//		마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+//		completion의 길이는 participant의 길이보다 1 작습니다.
+//		참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+//		참가자 중에는 동명이인이 있을 수 있습니다.
+
+		
+		
+		
+        //참가자 이름의 길이 초기변수설정
+		//알파벳 총 길이의 수는 26개
+		char [] characters = {'a','b','c','d','e','f','g','h','i','j','k',
+				'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+		};
+		
+		StringBuffer sb = new StringBuffer();
+		Random rand = new Random();
+		
+		for(int j =0; j < ) {
+			
+		}
+		
+		
+		
+        int participant_num = (int)(Math.random() * 100000) + 1;
+        
+        //랜덤 참가자의 이름 길이 
+        String[] participant = new String[participant_num];
+        
+        System.out.println(participant.length);
+        int i;
+        for(i= 0; i<participant.length; i++) {
+        	participant[i] = 
+        }
+         
+	}
+
+}
